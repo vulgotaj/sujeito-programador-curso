a40d4e1ced28ab17849aba45c61c72b222b9ae60
Felipe Tajima <felipebtajima05@gmail.com>
2025-08-18 18:23:49 -0300

    Requisições HTTP

---
+++ JAVASCRIPT/projeto11/script.js
@@ -0,0 +1,14 @@
+// let nomes = ["Mateus","Lucas","Jose"];
+
+// console.log(nomes.includes("Mateus"));
+
+// if(nomes.includes("Mateus")) {
+//     console.log("ESTA NA LISTA");
+// } else {
+//     console.log("NAO ESTÀ NA LISTA")
+// }
+
+let nome = "Matheus";
+
+console.log(nome.startsWith("Mat"));
+console.log(nome.endsWith("eus"));
