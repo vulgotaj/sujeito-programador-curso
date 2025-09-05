ade91523de0b8563c962f9dea05a323fe8c7c5f5
Felipe Tajima <felipebtajima05@gmail.com>
2025-09-05 13:46:50 -0300

    Listando os filmes

--- REACT/primeflix/src/pages/Home/index.js
+++ REACT/primeflix/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=b7562157e726fd357b11ab569dd5b149&language=pt-BR
 
 function Home() {
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -17,16 +20,35 @@ function Home() {
                 }
             })
 
-            console.log(response);
+            setFilmes(response.data.results.slice(0,10));
+            setLoading(false);
         }
 
         loadFilmes();
 
     }, [])
 
+    if(loading) {
+        return (
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>BEM VINDO A HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
