48facbb4176dae1a62322bfdb0d4c70f3d8f8a67
Felipe Tajima <felipebtajima05@gmail.com>
2025-10-27 22:29:19 -0300

    funcoes etc

---
+++ TYPESCRIPT/projeto01/src/functions/function.ts
@@ -0,0 +1,24 @@
+// function login(username: string): boolean | string {
+//     let message = 'Bem vindo ' + username;
+
+//     console.log(message);
+
+//     return username
+// }
+
+// const usernameLogin = login("matheus");
+
+// console.log(usernameLogin)
+
+let n1: number = 10;
+let n2: number = 25;
+
+function soma(valor1: number, valor2: number): number {
+
+    let soma = valor1 + valor2
+
+    return soma
+
+}
+
+console.log(soma(n1, n2));
